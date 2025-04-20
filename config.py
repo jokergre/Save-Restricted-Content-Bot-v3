8f811aaa1397c5e408f1bbb7a54b35cd06bc0a36
jokergre <104120002+jokergre@users.noreply.github.com>
2025-04-20 13:05:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,27 +7,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# VPS --- FILL COOKIES 🍪 in """ ... """ 
+INST_COOKIES = """"""
 
-INST_COOKIES = """
-# wtite up here insta cookies
-"""
+YTUB_COOKIES = """"""
 
-YTUB_COOKIES = """
-# write here yt cookies
-"""
-
-API_ID = os.getenv("API_ID", "")
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_DB = os.getenv("MONGO_DB", "")
-OWNER_ID = list(map(int, os.getenv("OWNER_ID", "").split())) # list seperated via space
+API_ID = os.getenv("API_ID", "25316666")
+API_HASH = os.getenv("API_HASH", "804e92d43aa2d7a2a0a552b46f38646a")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8054346385:AAFTYzkfhDQ3y2C6Vs2EdakJj4TCkUL7EMI")
+MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://creativexanas:Axbxcxdx1900@cluster0.w870qby.mongodb.net/?retryWrites=true&w=majority")
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "7314813409").split()))
 DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
-STRING = os.getenv("STRING", None) # optional
-LOG_GROUP = int(os.getenv("LOG_GROUP", "-1001234456")) # optional with -100
-FORCE_SUB = int(os.getenv("FORCE_SUB", "-10012345567")) # optional with -100
-MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
-IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
+STRING = os.getenv("STRING", '')
+LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002338464600"))
+FORCE_SUB = int(os.getenv("FORCE_SUB", "-1002586263929"))
+MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq")
+IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F")
 YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
 INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
 FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
